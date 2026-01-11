a3d68a4d421c1e07378a9ef682a87c7b327f10aa
Sulaiman Ahmad <sulibot@gmail.com>
2026-01-11 00:52:06 -0800

    working VM bgp

--- terraform/infra/modules/talos_config/main.tf
+++ terraform/infra/modules/talos_config/main.tf
@@ -151,7 +151,16 @@ data "talos_machine_configuration" "controlplane" {
         }
         sysctls  = local.common_sysctls
         features = local.common_features
-        kubelet = {}
+        kubelet = {
+          resolvConf = "/etc/resolv.conf.kubelet"
+        }
+        files = [
+          {
+            content = "nameserver 169.254.116.108\n"
+            path    = "/etc/resolv.conf.kubelet"
+            op      = "create"
+          }
+        ]
       }
       cluster = {
         allowSchedulingOnControlPlanes = false
@@ -224,8 +233,14 @@ data "talos_machine_configuration" "worker" {
             "fd00:${var.cluster_id}:96::a", # IPv6 DNS service IP (10th IP in service CIDR)
             "10.${var.cluster_id}.96.10"    # IPv4 DNS service IP (10th IP in service CIDR)
           ]
+          resolvConf = "/etc/resolv.conf.kubelet"
         }
         files = [
+          {
+            content = "nameserver 169.254.116.108\n"
+            path    = "/etc/resolv.conf.kubelet"
+            op      = "create"
+          },
           {
             op      = "create"
             path    = "/etc/cri/conf.d/20-customization.part"
